b5989734d770138e98ccaa286d4cb10f0c37c67a
dimitrije97 <dimitrijesalic@yahoo.com>
2020-03-21 19:24:11 +0100

    Uradjen login i popunjena baza pacijentima

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into patient (id, active) values
+('e14cf27c-cd22-4004-b0f7-c94fcd13aabf', true),
+('170dccaf-cf4d-4e9e-aa4e-1e3498d17a97', true),
+('d1b5fb6a-0bdd-4df1-8fda-aef7560d7828', true),
+('7086dad5-bc13-4977-a7ff-a36d58e068cf', true),
+('fa9594c4-430f-420e-a7f0-a3055b7b9aa3', true);
