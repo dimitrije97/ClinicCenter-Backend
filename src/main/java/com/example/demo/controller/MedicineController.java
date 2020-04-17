5f78803b6dacb70abf4d060e4ded5aac5de54e28
milicasvonja <milica.svonja997@yahoo.com>
2020-04-17 20:36:09 +0200

    Izmena postojeceg leka

--- src/main/java/com/example/demo/controller/MedicineController.java
+++ src/main/java/com/example/demo/controller/MedicineController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.request.CreateMedicineRequest;
+import com.example.demo.dto.request.UpdateMedicineRequest;
 import com.example.demo.dto.response.MedicineResponse;
 import com.example.demo.service.IMedicineService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicines")
@@ -21,5 +19,10 @@ public class MedicineController {
     @PostMapping
     public MedicineResponse createMedicine(@RequestBody CreateMedicineRequest request) throws Exception {
         return _medicineService.createMedicine(request);
+    }
+
+    @PutMapping
+    public MedicineResponse updateMedicine(@RequestBody UpdateMedicineRequest request) throws Exception {
+        return _medicineService.updateMedicine(request);
     }
 }
