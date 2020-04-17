e12b9db0ef6e063701e840934ef02c2cdd5cfc01
milicasvonja <milica.svonja997@yahoo.com>
2020-04-17 19:56:22 +0200

    Napravljen entitet sifatnika lekova

---
+++ src/main/java/com/example/demo/entity/Medicine.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medicine extends BaseEntity {
+
+    @Column(unique = true)
+    private String name;
+}
